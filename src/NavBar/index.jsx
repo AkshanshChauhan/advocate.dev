f3e2c16b43ba40be30d4906114f4e2716e9c677e
akshansh <chauhanakshansh57@gmail.com>
2022-12-04 18:26:22 +0530

    fourteen

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -2,9 +2,17 @@ import './navbar.scss';
 import React, { useState } from "react";
 import {Link} from "react-router-dom";
 import mainLogo from '../Images/mainLogo.svg';
+import mpic1 from "./imgs/mpic1.svg";
+import mpic2 from "./imgs/mpic2.svg";
+import mpic3 from "./imgs/mpic3.svg";
+import mpic4 from "./imgs/mpic4.svg";
+import mpic5 from "./imgs/mpic5.svg";
+import mpic6 from "./imgs/mpic6.svg";
+import mpic7 from "./imgs/mpic7.svg";
 
 function NavBar() {
     const [pos, setPos] = useState("");
+    const [myProfVis, setMyProfVis] = useState(true);
     window.onscroll = ()=> setPos(window.scrollY);
     return(
         <div className="navbar" style={{top: pos>200 ? "-120px" : "0px"}}>
@@ -17,7 +25,18 @@ function NavBar() {
                 <Link className="navLinks" to={"/lawyers"}>LAWYERS</Link>
                 <Link className="navLinks" to={"/affidavit"}>AFFIDAVIT</Link>
                 <Link className="navLinks" to={"/contact"}>CONTACT</Link>
-                <Link className="navLinks" to={"/myprofile"}>MY PROFILE</Link>
+                <div className="navLinks" style={{cursor: 'pointer'}} onMouseOver={()=>setMyProfVis(false)}>MY PROFILE
+                    <div className="myProfileLinks" style={myProfVis ? {display: 'none'} : {display: 'grid'}} onMouseLeave={()=>setMyProfVis(true)}>
+                        <Link className="myProfileLink" to={"/myprofile/edit-profile/"}><img src={mpic1} alt="myProfileIcon1" width={25} /> Edit Profile</Link>
+                        <Link className="myProfileLink" to={"/myprofile/payments/"}><img src={mpic2} alt="myProfileIcon2" width={25} /> Payments</Link>
+                        <Link className="myProfileLink" to={"/myprofile/help-and-support/"}><img src={mpic3} alt="myProfileIcon3" width={25} /> Help & Support</Link>
+                        <Link className="myProfileLink" to={"/myprofile/wishlist/"}><img src={mpic4} alt="myProfileIcon4" width={25} /> Wishlist</Link>
+                        <Link className="myProfileLink" to={"/myprofile/wallet/"}><img src={mpic5} alt="myProfileIcon5" width={25} /> Wallet</Link>
+                        <Link className="myProfileLink" to={"/myprofile/my-loyalty-status/"}><img src={mpic6} alt="myProfileIcon6" width={25} /> My Loyalty Status</Link>
+                        <Link className="myProfileLink" to={"/myprofile/recent-services/"}><img src={mpic7} alt="myProfileIcon7" width={25} /> Recent Services</Link>
+                        <button className="myProfileBtn">Join as Advocate</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
